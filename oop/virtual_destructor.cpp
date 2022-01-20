d3fb277881399967995b7cfb78dcb5d64a1e75e0
mariya12290 <kumar.mariya12@gmail.com>
2022-01-20 10:32:43 +0100

    added virtual destructor

--- oop/virtual_destructor.cpp
+++ oop/virtual_destructor.cpp
@@ -8,8 +8,12 @@ class Base{
         std::cout<<"Base class constructor "<<std::endl;
     }
 
-    ~Base(){
-        std::cout<<"Base class destructor "<<std::endl;
+    // ~Base(){
+    //     std::cout<<"Base class destructor "<<std::endl;
+    // }
+
+    virtual ~Base(){
+        std::cout<<"Base class virtual destructor "<<std::endl;
     }
 };
 
@@ -21,10 +25,14 @@ class Derived : public Base{
         arr = new int[static_cast<std::size_t>(N)];
 
     }
-    ~Derived(){
+    // ~Derived(){
 
-        std::cout<<"Derived class Destructor "<<std::endl;
-        delete [] arr;
+    //     std::cout<<"Derived class Destructor "<<std::endl;
+    //     delete [] arr;
+    // }
+
+    virtual ~Derived(){
+        std::cout<<"Derived class virtual destructor "<<std::endl;
     }
     private:
     int* arr;
@@ -38,7 +46,9 @@ int main(){
 
 Base* b = new Derived(10);
 
-delete b;
+delete b;  
+/** Without virtaul, Derived destructor is not called , so no de allocation of memory */
+
 
 
     return 0;
